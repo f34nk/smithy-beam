caf9bab0cdf93e9fb750f5d9aa191e561e82e1d5
Frank Eickhoff <info@frankeickhoff.de>
2026-04-12 15:29:09 +0200

    feat(core): add IR enums ErrorCodeStrategy and Role

---
+++ codegen-core/src/main/java/io/smithy/beam/core/ir/ErrorCodeStrategy.java
@@ -0,0 +1,5 @@
+package io.smithy.beam.core.ir;
+
+public enum ErrorCodeStrategy {
+    REST_JSON, AWS_JSON, AWS_QUERY, REST_XML
+}
---
+++ codegen-core/src/main/java/io/smithy/beam/core/ir/Role.java
@@ -0,0 +1,5 @@
+package io.smithy.beam.core.ir;
+
+public enum Role {
+    CLIENT, SERVER
+}
