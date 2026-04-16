d28dd5b3d34ee139a6f8e32fa6a2c7d3c6cce0f5
Frank Eickhoff <info@frankeickhoff.de>
2026-04-16 11:15:03 +0200

    build(elixir): bundle runtime-elixir into JAR and add smithy-aws-traits test dep

--- codegen-elixir/build.gradle.kts
+++ codegen-elixir/build.gradle.kts
@@ -1,7 +1,16 @@
+import org.gradle.api.tasks.Copy
+
 dependencies {
     api(project(":codegen-core"))
     implementation(project(":protocol-analyzer"))
     testImplementation(rootProject.libs.junit.api)
     testImplementation(rootProject.libs.assertj)
+    testImplementation(rootProject.libs.smithy.aws.traits)
     testRuntimeOnly(rootProject.libs.junit.engine)
+}
+
+tasks.named<Copy>("processResources") {
+    from(rootProject.layout.projectDirectory.dir("runtime-elixir")) {
+        into("META-INF/smithy-beam/runtime/elixir")
+    }
 }
