80bd5500ee36c1ac219db797d2ec84799c305d2c
Frank Eickhoff <info@frankeickhoff.de>
2026-04-19 12:44:50 +0200

    fix: correct CodeSection import in all Erlang code section records
    CodeSection lives in smithy-utils, not smithy-codegen-core. All ten
    section records had the wrong import, preventing compilation once the
    integration interceptors started referencing them.

--- codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/sections/StructTypeSection.java
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/sections/StructTypeSection.java
@@ -1,6 +1,6 @@
 package io.smithy.beam.erlang.codegen.sections;
 
-import software.amazon.smithy.codegen.core.CodeSection;
+import software.amazon.smithy.utils.CodeSection;
 import software.amazon.smithy.model.shapes.StructureShape;
 
 public record StructTypeSection(StructureShape structure) implements CodeSection {}
