7eb475d18dfe89b61bd82fa1f466069fd34e37fa
Frank Eickhoff <info@frankeickhoff.de>
2026-04-19 14:14:38 +0200

    feat: add test dependencies to codegen-erlang build

--- codegen/codegen-erlang/build.gradle.kts
+++ codegen/codegen-erlang/build.gradle.kts
@@ -2,6 +2,10 @@ dependencies {
     api(project(":codegen:codegen-core"))
     api(libs.smithy.codegen.core)
     api(libs.smithy.aws.traits)
+
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testImplementation(libs.assertj.core)
 }
 
 // Bundle hand-written Erlang runtime modules into the JAR.
