62dc60928baaeda6819a8b5a5b88d84f865eedc8
Frank Eickhoff <info@frankeickhoff.de>
2026-04-19 19:29:40 +0200

    test(codegen-test): add erlang client snapshot goldens

---
+++ codegen/codegen-test/src/test/resources/snapshots/erlang/client/weather_client_types.hrl
@@ -0,0 +1,28 @@
+-module(weather_client_types).
+
+-record(get_current_time_input, {}).
+-type get_current_time_input() :: #get_current_time_input{}.
+
+
+-record(get_current_time_output, {
+    time :: integer()
+}).
+-type get_current_time_output() :: #get_current_time_output{}.
+
+
+-record(get_forecast_input, {
+    city_id :: binary()
+}).
+-type get_forecast_input() :: #get_forecast_input{}.
+
+
+-record(get_forecast_output, {
+    chance_of_rain :: float()
+}).
+-type get_forecast_output() :: #get_forecast_output{}.
+
+
+-record(no_such_resource_error, {
+    resource_type :: binary()
+}).
+-type no_such_resource_error() :: #no_such_resource_error{}.
