ee2ff4abf8a5f99a4b373b2a372d1d6655ae8ac6
Frank Eickhoff <info@frankeickhoff.de>
2026-04-19 10:41:20 +0200

    feat: add restJson1 and restXml protocol integrations

---
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/protocol/ErlangRestXmlIntegration.java
@@ -0,0 +1,17 @@
+package io.smithy.beam.erlang.codegen.protocol;
+
+import io.smithy.beam.erlang.codegen.DefaultErlangProtocolIntegration;
+import software.amazon.smithy.model.shapes.ShapeId;
+
+/**
+ * Erlang codegen integration for the {@code aws.protocols#restXml} protocol.
+ *
+ * <p>Adds {@code SMITHY_XML} and {@code SMITHY_HTTP_CLIENT} runtime dependencies.
+ */
+public final class ErlangRestXmlIntegration extends DefaultErlangProtocolIntegration {
+
+    @Override
+    public ShapeId protocolId() {
+        return ShapeId.from("aws.protocols#restXml");
+    }
+}
