f5f9e4a406ee6f04c884919a1250a8a8881cec71
Frank Eickhoff <info@frankeickhoff.de>
2026-04-19 19:29:40 +0200

    test(codegen-test): add elixir client snapshot goldens (service + types)

---
+++ codegen/codegen-test/src/test/resources/snapshots/elixir/client/weather_client.ex
@@ -0,0 +1,10 @@
+defmodule Weather.Client do
+    def get_current_time(config, input) do
+        {:error, :not_implemented}
+    end
+
+    def get_forecast(config, input) do
+        {:error, :not_implemented}
+    end
+
+end
