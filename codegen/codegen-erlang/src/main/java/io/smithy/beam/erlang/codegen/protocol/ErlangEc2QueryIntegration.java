2d98ba4474afb4b3093b2975cdc7c218ea2ecb58
Frank Eickhoff <info@frankeickhoff.de>
2026-04-19 10:41:20 +0200

    feat: add awsQuery and ec2Query protocol integrations

---
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/protocol/ErlangEc2QueryIntegration.java
@@ -0,0 +1,17 @@
+package io.smithy.beam.erlang.codegen.protocol;
+
+import io.smithy.beam.erlang.codegen.DefaultErlangProtocolIntegration;
+import software.amazon.smithy.model.shapes.ShapeId;
+
+/**
+ * Erlang codegen integration for the {@code aws.protocols#ec2Query} protocol.
+ *
+ * <p>Adds {@code SMITHY_QUERY} and {@code SMITHY_HTTP_CLIENT} runtime dependencies.
+ */
+public final class ErlangEc2QueryIntegration extends DefaultErlangProtocolIntegration {
+
+    @Override
+    public ShapeId protocolId() {
+        return ShapeId.from("aws.protocols#ec2Query");
+    }
+}
