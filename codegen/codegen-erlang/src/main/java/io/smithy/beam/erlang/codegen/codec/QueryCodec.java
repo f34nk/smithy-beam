1b088779ad59ea97cca91c8e1fa8e21f069d1d5c
Frank Eickhoff <info@frankeickhoff.de>
2026-04-20 13:58:06 +0200

    fix(codegen-erlang): terminate QueryCodec error decode with trailing period

--- codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/codec/QueryCodec.java
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/codec/QueryCodec.java
@@ -89,6 +89,6 @@ public final class QueryCodec implements ErlangCodec {
         w.indent();
         w.write("_:_ -> {error, {http_error, StatusCode, Body}}");
         w.dedent();
-        w.closeBlock("end");
+        w.closeBlock("end.");
     }
 }
