dd736888cf82bd3df577e28b7443f02d7a5e581c
Frank Eickhoff <info@frankeickhoff.de>
2026-04-20 11:44:17 +0200

    feat(codegen-elixir): add CodeSection records for spec, validation, request, response, error, pagination, waiters, event streams, and server dispatch/deserialize/serialize/impl

---
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/codegen/sections/OperationSpecSection.java
@@ -0,0 +1,6 @@
+package io.smithy.beam.elixir.codegen.sections;
+
+import software.amazon.smithy.model.shapes.OperationShape;
+import software.amazon.smithy.utils.CodeSection;
+
+public record OperationSpecSection(OperationShape operation) implements CodeSection {}
