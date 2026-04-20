1f85693a3201d55d602bd91623e1eec752692124
Frank Eickhoff <info@frankeickhoff.de>
2026-04-20 19:36:21 +0200

    test(codegen-test): refresh elixir client snapshot with operation @spec lines

--- codegen/codegen-test/src/test/resources/snapshots/elixir/client/weather_client.ex
+++ codegen/codegen-test/src/test/resources/snapshots/elixir/client/weather_client.ex
@@ -1,8 +1,12 @@
 defmodule Weather.Client do
+  @spec get_current_time(map(), GetCurrentTimeInput.t()) ::
+    {:ok, GetCurrentTimeOutput.t()} | {:error, term()}
   def get_current_time(config, input) do
     {:error, :not_implemented}
   end
 
+  @spec get_forecast(map(), GetForecastInput.t()) ::
+    {:ok, GetForecastOutput.t()} | {:error, NoSuchResourceError.t()}
   def get_forecast(config, input) do
     {:error, :not_implemented}
   end
