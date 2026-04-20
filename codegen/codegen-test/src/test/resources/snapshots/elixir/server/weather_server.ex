c2c4c53f2ccfdb72e65f62317dfa8552c5c9f4e5
Frank Eickhoff <info@frankeickhoff.de>
2026-04-20 21:31:22 +0200

    test(codegen-test): refresh elixir server snapshot with @spec / @callback lines

--- codegen/codegen-test/src/test/resources/snapshots/elixir/server/weather_server.ex
+++ codegen/codegen-test/src/test/resources/snapshots/elixir/server/weather_server.ex
@@ -1,11 +1,19 @@
 defmodule Weather.Server do
   @behaviour SmithyBeam.Handler
+  @spec handle_get_current_time(request :: term(), state :: term()) :: {:ok, term()} | {:error, term()}
   def handle_get_current_time(request, state) do
     {:error, :not_implemented}
   end
 
+  @callback get_current_time(input :: GetCurrentTimeInput.t(), context :: term()) ::
+    {:ok, GetCurrentTimeOutput.t()} | {:error, term()}
+
+  @spec handle_get_forecast(request :: term(), state :: term()) :: {:ok, term()} | {:error, term()}
   def handle_get_forecast(request, state) do
     {:error, :not_implemented}
   end
+
+  @callback get_forecast(input :: GetForecastInput.t(), context :: term()) ::
+    {:ok, GetForecastOutput.t()} | {:error, NoSuchResourceError.t()}
 
 end
