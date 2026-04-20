086454d327e37b2347b1c890b6b1c6bd75aff1cd
Frank Eickhoff <info@frankeickhoff.de>
2026-04-20 13:58:06 +0200

    refactor(codegen-erlang): wire ErlangAwsQueryIntegration to QueryCodec + RpcTransport

--- codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/protocol/ErlangAwsQueryIntegration.java
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/codegen/protocol/ErlangAwsQueryIntegration.java
@@ -1,14 +1,16 @@
 package io.smithy.beam.erlang.codegen.protocol;
 
 import io.smithy.beam.erlang.codegen.DefaultErlangProtocolIntegration;
-import io.smithy.beam.erlang.codegen.ErlangDependency;
-import java.util.List;
+import io.smithy.beam.erlang.codegen.codec.ErlangCodec;
+import io.smithy.beam.erlang.codegen.codec.ErlangTransport;
+import io.smithy.beam.erlang.codegen.codec.QueryCodec;
+import io.smithy.beam.erlang.codegen.http.RpcTransport;
 import software.amazon.smithy.model.shapes.ShapeId;
 
 /**
  * Erlang codegen integration for the {@code aws.protocols#awsQuery} protocol.
  *
- * <p>Adds {@code SMITHY_QUERY} and {@code SMITHY_HTTP_CLIENT} runtime dependencies.
+ * <p>Pairs the {@link QueryCodec} with the {@link RpcTransport}.
  */
 public final class ErlangAwsQueryIntegration extends DefaultErlangProtocolIntegration {
 
@@ -18,7 +20,12 @@ public final class ErlangAwsQueryIntegration extends DefaultErlangProtocolIntegr
     }
 
     @Override
-    protected List<ErlangDependency> protocolDependencies() {
-        return List.of(ErlangDependency.SMITHY_QUERY, ErlangDependency.SMITHY_HTTP_CLIENT);
+    protected ErlangCodec codec() {
+        return new QueryCodec();
+    }
+
+    @Override
+    protected ErlangTransport transport() {
+        return new RpcTransport();
     }
 }
