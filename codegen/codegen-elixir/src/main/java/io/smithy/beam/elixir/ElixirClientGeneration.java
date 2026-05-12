079540563a387262e954ac79a0216330403dc844
Frank Eickhoff <info@frankeickhoff.de>
2026-05-12 20:41:29 +0200

    feat(elixir): apply shared CodegenDirector transforms in Elixir client and server generation

--- codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirClientGeneration.java
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirClientGeneration.java
@@ -1,5 +1,6 @@
 package io.smithy.beam.elixir;
 
+import io.smithy.beam.core.BeamCodegenTransforms;
 import io.smithy.beam.core.BeamSettings;
 import software.amazon.smithy.build.PluginContext;
 import software.amazon.smithy.codegen.core.directed.CodegenDirector;
@@ -27,8 +28,7 @@ public final class ElixirClientGeneration {
         BeamSettings settings = runner.settings(BeamSettings.class, context.getSettings());
         runner.service(settings.resolveService(context.getModel()));
 
-        runner.performDefaultCodegenTransforms();
-        runner.createDedicatedInputsAndOutputs();
+        BeamCodegenTransforms.applySharedCodegenTransforms(runner, settings);
 
         runner.run();
     }
