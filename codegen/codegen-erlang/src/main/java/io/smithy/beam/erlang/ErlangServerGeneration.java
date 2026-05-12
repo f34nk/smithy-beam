cbaf279240d6731f21796c4c4a8044b1fbc2445f
Frank Eickhoff <info@frankeickhoff.de>
2026-05-12 20:41:28 +0200

    feat(erlang): apply shared CodegenDirector transforms in Erlang client and server generation

--- codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/ErlangServerGeneration.java
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/ErlangServerGeneration.java
@@ -1,5 +1,6 @@
 package io.smithy.beam.erlang;
 
+import io.smithy.beam.core.BeamCodegenTransforms;
 import io.smithy.beam.core.BeamSettings;
 import software.amazon.smithy.build.PluginContext;
 import software.amazon.smithy.codegen.core.directed.CodegenDirector;
@@ -27,8 +28,7 @@ public final class ErlangServerGeneration {
         BeamSettings settings = runner.settings(BeamSettings.class, context.getSettings());
         runner.service(settings.resolveService(context.getModel()));
 
-        runner.performDefaultCodegenTransforms();
-        runner.createDedicatedInputsAndOutputs();
+        BeamCodegenTransforms.applySharedCodegenTransforms(runner, settings);
 
         runner.run();
     }
