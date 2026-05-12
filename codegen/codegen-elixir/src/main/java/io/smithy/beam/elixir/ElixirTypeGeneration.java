712dcee0139d56f387499fe9e6b0d78855b69d3c
Frank Eickhoff <info@frankeickhoff.de>
2026-05-12 20:41:29 +0200

    feat(elixir): apply shared CodegenDirector transforms in Elixir type generation

--- codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirTypeGeneration.java
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirTypeGeneration.java
@@ -1,5 +1,6 @@
 package io.smithy.beam.elixir;
 
+import io.smithy.beam.core.BeamCodegenTransforms;
 import io.smithy.beam.core.BeamSettings;
 import software.amazon.smithy.build.PluginContext;
 import software.amazon.smithy.codegen.core.directed.CodegenDirector;
@@ -21,8 +22,7 @@ public final class ElixirTypeGeneration {
         BeamSettings settings = runner.settings(BeamSettings.class, context.getSettings());
         runner.service(settings.resolveService(context.getModel()));
 
-        runner.performDefaultCodegenTransforms();
-        runner.createDedicatedInputsAndOutputs();
+        BeamCodegenTransforms.applySharedCodegenTransforms(runner, settings);
 
         runner.run();
     }
