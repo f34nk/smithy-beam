8a33d39aafced1ef0d52e0d1194a3e661bf3fbae
Frank Eickhoff <info@frankeickhoff.de>
2026-05-20 15:35:25 +0200

    feat(elixir): add section helpers on ElixirWriter
    Adds push helpers mapping to ElixirWriterSections for DirectedCodegen call sites.
    Keeps integration targets stable while preserving paired popState discipline on each path.

--- codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirWriter.java
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirWriter.java
@@ -1,6 +1,7 @@
 package io.smithy.beam.elixir;
 
 import software.amazon.smithy.codegen.core.SymbolWriter;
+import software.amazon.smithy.utils.CodeSection;
 
 /**
  * Code writer for Elixir .ex type files.
@@ -26,5 +27,29 @@ public final class ElixirWriter extends SymbolWriter<ElixirWriter, ElixirImports
 
     public static Factory<ElixirWriter> factory(String namespace) {
         return (filename, ns) -> new ElixirWriter(filename, namespace);
+    }
+
+    public void pushGeneratedDocumentationSection() {
+        pushState((CodeSection) new ElixirWriterSections.GeneratedDocumentation());
+    }
+
+    public void pushModuleHeaderSection() {
+        pushState((CodeSection) new ElixirWriterSections.ModuleHeader());
+    }
+
+    public void pushDependenciesSection() {
+        pushState((CodeSection) new ElixirWriterSections.Dependencies());
+    }
+
+    public void pushProtocolHookSection() {
+        pushState((CodeSection) new ElixirWriterSections.ProtocolHook());
+    }
+
+    public void pushTransportHookSection() {
+        pushState((CodeSection) new ElixirWriterSections.TransportHook());
+    }
+
+    public void pushOperationBodySection() {
+        pushState((CodeSection) new ElixirWriterSections.OperationBody());
     }
 }
