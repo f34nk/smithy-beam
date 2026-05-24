b114448cfb554c4719ddbb79dd8f213925eb9a2d
Frank Eickhoff <info@frankeickhoff.de>
2026-05-24 12:51:50 +0200

    test: add AWS protocol traits to the Gradle version catalog and tests
    Adds smithy-aws-traits to the version catalog and codegen-test classpath for REST JSON models.
    Unblocks assembling services that attach aws.protocols traits such as restJson1 in fixtures.

--- codegen/codegen-test/build.gradle.kts
+++ codegen/codegen-test/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
     testImplementation(libs.smithy.model)
     testImplementation(libs.smithy.codegen.core)
     testImplementation(libs.smithy.build)
+    testImplementation(libs.smithy.aws.traits)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.assertj.core)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
