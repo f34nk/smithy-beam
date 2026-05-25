296f444b4978725e9bdcb93a8792e4a50b7ffc8c
Frank Eickhoff <info@frankeickhoff.de>
2026-05-25 22:35:43 +0200

    feat(elixir): elixir runtime types module stub
    Adds ElixirRuntimeTypesEmitter writing HttpRequest and HttpResponse structs for generated clients.
    Mirrors Erlang runtime carrier concepts for Elixir clients behind a generated module path.

---
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirRuntimeTypesEmitter.java
@@ -0,0 +1,35 @@
+package io.smithy.beam.elixir;
+
+import io.smithy.beam.core.BeamSettings;
+import software.amazon.smithy.model.shapes.ServiceShape;
+
+public final class ElixirRuntimeTypesEmitter {
+
+    private ElixirRuntimeTypesEmitter() {}
+
+    public static String modulePath(BeamSettings settings, ServiceShape service) {
+        String app = settings.resolveModule(service.getId().getNamespace());
+        return app + "_runtime_types.ex";
+    }
+
+    public static void writeBody(ElixirWriter writer, String moduleName) {
+        writer.write("@moduledoc \"\"\"");
+        writer.write("Generated HTTP and client runtime types for Smithy service clients.");
+        writer.write("\"\"\"");
+        writer.write("defmodule $L do", moduleName);
+        writer.indent();
+        writer.write("@type http_request :: %__MODULE__.HttpRequest{}");
+        writer.write("defmodule HttpRequest do");
+        writer.indent();
+        writer.write("defstruct method: \"GET\", path: \"/\", query: %{}, headers: [], body: \"\"");
+        writer.dedent();
+        writer.write("end");
+        writer.write("defmodule HttpResponse do");
+        writer.indent();
+        writer.write("defstruct status: 200, headers: [], body: \"\"");
+        writer.dedent();
+        writer.write("end");
+        writer.dedent();
+        writer.write("end");
+    }
+}
