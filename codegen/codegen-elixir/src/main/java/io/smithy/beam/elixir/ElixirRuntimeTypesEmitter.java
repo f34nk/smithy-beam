babe3a7c4e9465d58a7861325fcc89f23e38cca8
Frank Eickhoff <info@frankeickhoff.de>
2026-05-26 16:53:07 +0200

    refactor(elixir): drop duplicated runtime types path helper
    Runtime types output path is resolved by callers via BeamElixirLayout.

--- codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirRuntimeTypesEmitter.java
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirRuntimeTypesEmitter.java
@@ -1,16 +1,8 @@
 package io.smithy.beam.elixir;
 
-import io.smithy.beam.core.BeamSettings;
-import software.amazon.smithy.model.shapes.ServiceShape;
-
 public final class ElixirRuntimeTypesEmitter {
 
     private ElixirRuntimeTypesEmitter() {}
-
-    public static String modulePath(BeamSettings settings, ServiceShape service) {
-        String app = settings.resolveModule(service.getId().getNamespace());
-        return app + "_runtime_types.ex";
-    }
 
     public static void writeBody(ElixirWriter writer, String moduleName) {
         writer.write("defmodule $L do", moduleName);
