76ace92ddd5e0237ef51faae7ced3baa8312adea
Frank Eickhoff <info@frankeickhoff.de>
2026-05-28 19:42:32 +0200

    test(elixir): cover generated paginator walking all pages

---
+++ examples/elixir/basic/test/basic_paginators_test.exs
@@ -0,0 +1,24 @@
+defmodule BasicPaginatorsTest do
+  use ExUnit.Case, async: true
+
+  alias Basic.ListBasicItemsInput
+
+  describe "paginate_list_basic_items/2" do
+    test "collects all pages through the generated client" do
+      config = %{base_url: "https://api.example", http_client: BasicHttpMock}
+      input = %ListBasicItemsInput{}
+
+      assert {:ok, items} = BasicPaginators.paginate_list_basic_items(config, input)
+      assert length(items) == 2
+      assert Enum.at(items, 0) == %{"name" => "alpha", "count" => 1}
+      assert Enum.at(items, 1) == %{"name" => "beta", "count" => 2}
+    end
+  end
+
+  describe "exports" do
+    test "defines paginate_list_basic_items/2 and /3" do
+      assert {:paginate_list_basic_items, 2} in BasicPaginators.__info__(:functions)
+      assert {:paginate_list_basic_items, 3} in BasicPaginators.__info__(:functions)
+    end
+  end
+end
