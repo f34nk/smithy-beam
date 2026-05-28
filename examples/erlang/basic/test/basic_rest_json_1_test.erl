4937fa2a0c02778aa495f5dcbcd9c6b288aff04f
Frank Eickhoff <info@frankeickhoff.de>
2026-05-28 20:15:55 +0200

    test(erlang): call decode_<op>_request/2 with a pre-built label map

--- examples/erlang/basic/test/basic_rest_json_1_test.erl
+++ examples/erlang/basic/test/basic_rest_json_1_test.erl
@@ -41,7 +41,21 @@ encode_omits_optional_fields_test() ->
     ?assertEqual(#{}, Req#http_request.query),
     ?assertEqual([{<<"Content-Type">>, <<"application/json">>}], Req#http_request.headers).
 
-%% decode_get_type_closure_request/1
+%% decode_get_type_closure_request/2
+
+decode_get_type_closure_request_uses_label_map_test() ->
+    Req = #http_request{
+        method = <<"GET">>,
+        path = <<"/types/widget">>,
+        query = #{<<"verbose">> => <<"true">>},
+        headers = [{<<"X-Request-Tag">>, <<"trace-1">>}],
+        body = <<>>
+    },
+    LabelMap = #{<<"name">> => <<"widget">>},
+    Input = basic_rest_json_1:decode_get_type_closure_request(Req, LabelMap),
+    ?assertEqual(<<"widget">>, Input#get_type_closure_input.name),
+    ?assertEqual(true, Input#get_type_closure_input.verbose),
+    ?assertEqual(<<"trace-1">>, Input#get_type_closure_input.request_tag).
 
 decode_minimal_request_test() ->
     Req = #http_request{
@@ -51,7 +65,8 @@ decode_minimal_request_test() ->
         headers = [{<<"Content-Type">>, <<"application/json">>}],
         body = <<>>
     },
-    Input = basic_rest_json_1:decode_get_type_closure_request(Req),
+    LabelMap = #{<<"name">> => <<"widget">>},
+    Input = basic_rest_json_1:decode_get_type_closure_request(Req, LabelMap),
     ?assertEqual(<<"widget">>, Input#get_type_closure_input.name),
     ?assertEqual(undefined, Input#get_type_closure_input.verbose),
     ?assertEqual(undefined, Input#get_type_closure_input.request_tag).
@@ -64,7 +79,8 @@ decode_full_request_test() ->
         headers = [{<<"X-Request-Tag">>, <<"trace-1">>}],
         body = <<>>
     },
-    Input = basic_rest_json_1:decode_get_type_closure_request(Req),
+    LabelMap = #{<<"name">> => <<"widget">>},
+    Input = basic_rest_json_1:decode_get_type_closure_request(Req, LabelMap),
     ?assertEqual(<<"widget">>, Input#get_type_closure_input.name),
     ?assertEqual(true, Input#get_type_closure_input.verbose),
     ?assertEqual(<<"trace-1">>, Input#get_type_closure_input.request_tag).
@@ -77,7 +93,8 @@ decode_uri_decodes_path_label_test() ->
         headers = [],
         body = <<>>
     },
-    Input = basic_rest_json_1:decode_get_type_closure_request(Req),
+    LabelMap = #{<<"name">> => <<"hello world">>},
+    Input = basic_rest_json_1:decode_get_type_closure_request(Req, LabelMap),
     ?assertEqual(<<"hello world">>, Input#get_type_closure_input.name).
 
 %% decode_get_type_closure_response/1
