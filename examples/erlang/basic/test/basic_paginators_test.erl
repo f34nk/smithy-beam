7a0dac227022b7b2dbb45be75e855ab9ce1040d0
Frank Eickhoff <info@frankeickhoff.de>
2026-05-28 19:42:32 +0200

    test(erlang): cover generated paginator walking all pages

---
+++ examples/erlang/basic/test/basic_paginators_test.erl
@@ -0,0 +1,20 @@
+-module(basic_paginators_test).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("basic_types.hrl").
+
+paginate_list_basic_items_collects_all_pages_test() ->
+    Config = #{base_url => <<"https://api.example">>, http_client => basic_http_mock},
+    Input = #list_basic_items_input{},
+    ?assertEqual(
+        {ok, [
+            #{<<"name">> => <<"alpha">>, <<"count">> => 1},
+            #{<<"name">> => <<"beta">>, <<"count">> => 2}
+        ]},
+        basic_paginators:paginate_list_basic_items(Config, Input)
+    ).
+
+paginate_list_basic_items_exports_test() ->
+    Exports = basic_paginators:module_info(exports),
+    ?assert(lists:member({paginate_list_basic_items, 2}, Exports)),
+    ?assert(lists:member({paginate_list_basic_items, 3}, Exports)).
