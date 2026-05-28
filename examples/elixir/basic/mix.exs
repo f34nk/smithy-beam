f9aac5fcaee1abeea03976a1fe3523d63105f1d8
Frank Eickhoff <info@frankeickhoff.de>
2026-05-28 14:26:33 +0200

    chore(elixir): ignore test support scripts in ExUnit file scan

--- examples/elixir/basic/mix.exs
+++ examples/elixir/basic/mix.exs
@@ -7,6 +7,7 @@ defmodule Basic.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      test_ignore_filters: [~r/test\/support\//],
       deps: deps()
     ]
   end
