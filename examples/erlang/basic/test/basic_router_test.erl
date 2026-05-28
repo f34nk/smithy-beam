b0ae74fb93a960a9abc7eac8042d74e85e2f9c49
Frank Eickhoff <info@frankeickhoff.de>
2026-05-28 20:15:55 +0200

    test(erlang): cover router path selection for labeled and literal routes

--- examples/erlang/basic/test/basic_router_test.erl
+++ examples/erlang/basic/test/basic_router_test.erl
@@ -7,6 +7,29 @@
 %% Request flow: http_request -> basic_router:dispatch/2 -> codec decode -> Handler:handle_<op>/3.
 %% Pass basic_server as Handler to invoke the generated server stub.
 
+dispatch_routes_list_basic_items_before_type_prefix_test() ->
+    Req = #http_request{
+        method = <<"GET">>,
+        path = <<"/basic-items">>,
+        query = #{},
+        headers = [],
+        body = <<>>
+    },
+    ?assertEqual({error, not_implemented}, basic_router:dispatch(basic_server, Req)).
+
+dispatch_not_found_for_extra_type_segments_test() ->
+    Req = #http_request{
+        method = <<"GET">>,
+        path = <<"/types/a/b">>,
+        query = #{},
+        headers = [],
+        body = <<>>
+    },
+    ?assertEqual(
+        {error, {not_found, <<"GET">>, <<"/types/a/b">>}},
+        basic_router:dispatch(basic_server, Req)
+    ).
+
 dispatch_routes_to_basic_server_handler_test() ->
     Req = #http_request{
         method = <<"GET">>,
