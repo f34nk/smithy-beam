d4d1b961d7c93619dce31b83591f70f503f65dfb
Frank Eickhoff <info@frankeickhoff.de>
2026-05-28 22:38:43 +0200

    test: include shared runtime_types header in basic-client tests

--- examples/erlang/basic-client/test/basic_http_test.erl
+++ examples/erlang/basic-client/test/basic_http_test.erl
@@ -1,7 +1,7 @@
 -module(basic_http_test).
 
 -include_lib("eunit/include/eunit.hrl").
--include("basic_runtime_types.hrl").
+-include("runtime_types.hrl").
 
 dispatch_builds_url_without_query_test() ->
     Config = #{base_url => <<"https://api.example">>},
--- examples/erlang/basic-client/test/basic_rest_json_1_test.erl
+++ examples/erlang/basic-client/test/basic_rest_json_1_test.erl
@@ -2,7 +2,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 -include("basic_types.hrl").
--include("basic_runtime_types.hrl").
+-include("runtime_types.hrl").
 
 %% encode_get_type_closure_request/1
 
