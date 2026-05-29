590f440d5b9a6ee0c5bcb83497251ca2c9a8a118
Frank Eickhoff <info@frankeickhoff.de>
2026-05-29 17:57:13 +0200

    test: @jsonName trait uses trait value as wire key in codec
    Asserts codec encode and decode use the @jsonName value, not the member name,
    for JSON object keys in REST JSON 1 generated code.

---
+++ codegen/codegen-test/src/test/java/io/smithy/beam/test/JsonNameCodecTest.java
@@ -0,0 +1,65 @@
+package io.smithy.beam.test;
+
+import io.smithy.beam.erlang.ErlangClientPlugin;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.build.MockManifest;
+import software.amazon.smithy.build.PluginContext;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.node.ObjectNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JsonNameCodecTest {
+
+    private static final String MODEL = """
+            $version: "2"
+            namespace smithy.beam.test.jsonname
+
+            use aws.protocols#restJson1
+
+            @restJson1
+            service JsonNameService {
+                version: "2026"
+                operations: [GetItem]
+            }
+
+            @http(method: "GET", uri: "/items/{id}")
+            @readonly
+            operation GetItem {
+                input: GetItemInput
+                output: GetItemOutput
+            }
+
+            structure GetItemInput {
+                @required @httpLabel
+                id: String
+            }
+
+            structure GetItemOutput {
+                @jsonName("displayName")
+                name: String
+            }
+            """;
+
+    @Test
+    void jsonNameUsedAsWireKey() {
+        Model model = Model.assembler()
+                .addUnparsedModel("test.smithy", MODEL)
+                .discoverModels().assemble().unwrap();
+        MockManifest manifest = new MockManifest();
+        new ErlangClientPlugin().execute(PluginContext.builder()
+                .model(model).fileManifest(manifest)
+                .settings(ObjectNode.builder()
+                        .withMember("service",
+                                "smithy.beam.test.jsonname#JsonNameService")
+                        .withMember("edition", "2026")
+                        .withMember("protocol", "aws.protocols#restJson1")
+                        .build())
+                .build());
+
+        String codec = manifest.getFileString(
+                "json_name_service_rest_json_1.erl").orElse("");
+        assertThat(codec).contains("<<\"displayName\">>");
+        assertThat(codec).doesNotContain("<<\"name\">>");
+    }
+}
