a12d219df27ddb00d5c469e3b6ada74b4871739d
Frank Eickhoff <info@frankeickhoff.de>
2026-05-29 14:15:46 +0200

    test(erlang): expect service-scoped symbol names

--- codegen/codegen-erlang/src/test/java/io/smithy/beam/erlang/ErlangSymbolProviderTest.java
+++ codegen/codegen-erlang/src/test/java/io/smithy/beam/erlang/ErlangSymbolProviderTest.java
@@ -476,7 +476,7 @@ class ErlangSymbolProviderTest {
             ErlangSymbolProvider clientProvider = new ErlangSymbolProvider(
                     testSettings(), model, service, "example_client.erl", BeamCodegenKind.CLIENT);
             Symbol sym = clientProvider.toSymbol(service);
-            assertThat(sym.getName()).isEqualTo("example_client");
+            assertThat(sym.getName()).isEqualTo("example_service_client");
             assertThat(sym.getProperty("builtIn", Boolean.class)).contains(false);
             assertThat(sym.getDefinitionFile()).isEqualTo("example_client.erl");
         }
@@ -486,7 +486,7 @@ class ErlangSymbolProviderTest {
             ErlangSymbolProvider serverProvider = new ErlangSymbolProvider(
                     testSettings(), model, service, "example_server.erl", BeamCodegenKind.SERVER);
             Symbol sym = serverProvider.toSymbol(service);
-            assertThat(sym.getName()).isEqualTo("example_server");
+            assertThat(sym.getName()).isEqualTo("test_service_server");
             assertThat(sym.getProperty("builtIn", Boolean.class)).contains(false);
             assertThat(sym.getDefinitionFile()).isEqualTo("example_server.erl");
         }
