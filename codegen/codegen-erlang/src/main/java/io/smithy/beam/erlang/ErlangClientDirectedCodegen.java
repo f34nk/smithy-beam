0466426446bd00dfd163e7689303cefdb629a9de
Frank Eickhoff <info@frankeickhoff.de>
2026-05-31 19:42:01 +0200

    feat: Erlang AWS Query request encoder
    Emits form-urlencoded POST bodies with Action and Version parameters.

--- codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/ErlangClientDirectedCodegen.java
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/ErlangClientDirectedCodegen.java
@@ -9,6 +9,7 @@ import io.smithy.beam.core.BeamProtocolCodegen;
 import io.smithy.beam.core.BeamProtocolCodegenFactory;
 import io.smithy.beam.core.BeamAwsJson10ProtocolCodegen;
 import io.smithy.beam.core.BeamAwsJson11ProtocolCodegen;
+import io.smithy.beam.core.BeamAwsQueryProtocolCodegen;
 import io.smithy.beam.core.BeamRestJson1ProtocolCodegen;
 import io.smithy.beam.core.BeamProtocolResolver;
 import io.smithy.beam.core.BeamResourceIndex;
@@ -185,6 +186,10 @@ final class ErlangClientDirectedCodegen
                 && BeamAwsJson11ProtocolCodegen.AWS_JSON_1_1.equals(
                         ctx.protocolCodegen().protocolTraitId())) {
             ErlangAwsJson11Emitter.emitCodecModule(ctx, directive.shape());
+        } else if (ctx.protocolCodegen() != null
+                && BeamAwsQueryProtocolCodegen.AWS_QUERY.equals(
+                        ctx.protocolCodegen().protocolTraitId())) {
+            ErlangAwsQueryEmitter.emitCodecModule(ctx, directive.shape());
         }
 
         ErlangHttpDispatchEmitter.emit(ctx, service);
@@ -239,6 +244,8 @@ final class ErlangClientDirectedCodegen
                         || BeamAwsJson10ProtocolCodegen.AWS_JSON_1_0.equals(
                                 ctx.protocolCodegen().protocolTraitId())
                         || BeamAwsJson11ProtocolCodegen.AWS_JSON_1_1.equals(
+                                ctx.protocolCodegen().protocolTraitId())
+                        || BeamAwsQueryProtocolCodegen.AWS_QUERY.equals(
                                 ctx.protocolCodegen().protocolTraitId()));
 
         BeamDocumentation.forShape(op).ifPresent(doc -> {
