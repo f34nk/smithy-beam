0a299c82125c37efb25d6bf4fca0ab049f74bf44
Frank Eickhoff <info@frankeickhoff.de>
2026-05-31 19:46:15 +0200

    feat: EC2 Query protocol registration and ec2QueryName key mapping
    Prepares EC2-flavored form key names distinct from standard AWS Query.

--- codegen/codegen-core/src/main/java/io/smithy/beam/core/BeamAwsQueryFormEncoder.java
+++ codegen/codegen-core/src/main/java/io/smithy/beam/core/BeamAwsQueryFormEncoder.java
@@ -1,7 +1,10 @@
 package io.smithy.beam.core;
 
+import software.amazon.smithy.aws.traits.protocols.Ec2QueryNameTrait;
+import software.amazon.smithy.model.shapes.MemberShape;
 import software.amazon.smithy.model.shapes.OperationShape;
 import software.amazon.smithy.model.shapes.ServiceShape;
+import software.amazon.smithy.model.traits.XmlNameTrait;
 
 public final class BeamAwsQueryFormEncoder {
 
@@ -13,5 +16,26 @@ public final class BeamAwsQueryFormEncoder {
 
     public static String serviceVersion(ServiceShape service) {
         return service.getVersion();
+    }
+
+    public static String awsQueryFormKey(MemberShape member) {
+        return member.getTrait(XmlNameTrait.class)
+                .map(XmlNameTrait::getValue)
+                .orElseGet(() -> capitalizeFirst(member.getMemberName()));
+    }
+
+    public static String ec2QueryFormKey(MemberShape member) {
+        return member.getTrait(Ec2QueryNameTrait.class)
+                .map(Ec2QueryNameTrait::getValue)
+                .orElseGet(() -> member.getTrait(XmlNameTrait.class)
+                        .map(trait -> capitalizeFirst(trait.getValue()))
+                        .orElseGet(() -> capitalizeFirst(member.getMemberName())));
+    }
+
+    private static String capitalizeFirst(String name) {
+        if (name.isEmpty()) {
+            return name;
+        }
+        return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
 }
--- codegen/codegen-core/src/main/java/io/smithy/beam/core/BeamEc2QueryProtocolCodegen.java
+++ codegen/codegen-core/src/main/java/io/smithy/beam/core/BeamEc2QueryProtocolCodegen.java
@@ -1,10 +1,22 @@
 package io.smithy.beam.core;
 
+import software.amazon.smithy.codegen.core.CodegenContext;
+import software.amazon.smithy.model.shapes.OperationShape;
+import software.amazon.smithy.model.shapes.ServiceShape;
 import software.amazon.smithy.model.shapes.ShapeId;
 
-public final class BeamEc2QueryProtocolCodegen {
+public final class BeamEc2QueryProtocolCodegen implements BeamProtocolCodegen {
 
     public static final ShapeId EC2_QUERY = ShapeId.from("aws.protocols#ec2Query");
 
-    private BeamEc2QueryProtocolCodegen() {}
+    @Override
+    public ShapeId protocolTraitId() {
+        return EC2_QUERY;
+    }
+
+    @Override
+    public void emitOperationBindings(CodegenContext<?, ?, ?> ctx,
+            ServiceShape service, OperationShape operation) {
+        // Language emitters own codec function bodies.
+    }
 }
