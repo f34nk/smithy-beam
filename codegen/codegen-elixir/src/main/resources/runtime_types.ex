33d2cc80f36e1b215fa18a8fce66c0aab3646030
Frank Eickhoff <info@frankeickhoff.de>
2026-06-08 17:55:33 +0200

    fix elixir runtime_types template layout
    Align nested HttpRequest and HttpResponse modules with two-space writer
    indent and separate sibling defmodules with a blank line.

--- codegen/codegen-elixir/src/main/resources/runtime_types.ex
+++ codegen/codegen-elixir/src/main/resources/runtime_types.ex
@@ -2,6 +2,7 @@
 defmodule HttpRequest do
   defstruct method: "GET", path: "/", query: %{}, headers: [], body: "", host: nil, stream: nil
 end
+
 defmodule HttpResponse do
   defstruct status: 200, headers: [], body: "", stream: nil
 end
