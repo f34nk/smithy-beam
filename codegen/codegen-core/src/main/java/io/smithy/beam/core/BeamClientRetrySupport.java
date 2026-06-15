c964559fa322d9c060185127c3e551687c303c41
Frank Eickhoff <info@frankeickhoff.de>
2026-06-15 22:26:36 +0200

    feat add BeamClientRetrySupport for operation retry wiring
    Detect when an operation declares at least one modeled @retryable error.

---
+++ codegen/codegen-core/src/main/java/io/smithy/beam/core/BeamClientRetrySupport.java
@@ -0,0 +1,23 @@
+package io.smithy.beam.core;
+
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.OperationShape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.StructureShape;
+
+public final class BeamClientRetrySupport {
+
+    private BeamClientRetrySupport() {}
+
+    public static boolean operationHasRetryableErrors(Model model, OperationShape operation) {
+        for (ShapeId errorId : operation.getErrors()) {
+            StructureShape error = model.expectShape(errorId, StructureShape.class);
+            if (BeamRetryIndex.forError(error)
+                    .map(BeamRetryIndex.RetryInfo::retryable)
+                    .orElse(false)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
