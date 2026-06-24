3cead6ca3698e20bd1a6824a510de2eb9f721d88
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 16:42:47 +0200

    test: cover compact match-case binding rendering

--- codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlMatchTest.java
+++ codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlMatchTest.java
@@ -2,6 +2,8 @@ package io.smithy.beam.ir.erlang;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ErlMatchTest {
@@ -19,5 +21,25 @@ class ErlMatchTest {
                 new ErlVarPattern("Value"),
                 ErlCallLocal.callLocal("fetch"));
         assertThat(match.asString()).isEqualTo("Value = fetch()");
+    }
+
+    @Test
+    void matchCaseLines() {
+        ErlMatch match = ErlMatch.match(
+                new ErlVarPattern("Input1"),
+                ErlCase.caseExpr(
+                        ErlRecordAccess.recordAccess(
+                                ErlVar.var("Input"), "create_resource_input", "client_token"),
+                        ErlClause.clause(
+                                List.of(new ErlAtomPattern("undefined")),
+                                new ErlAtom("undefined")),
+                        ErlClause.clause(
+                                List.of(new ErlVarPattern("_")),
+                                ErlVar.var("Input"))));
+        assertThat(match.lines()).containsExactly(
+                "Input1 = case Input#create_resource_input.client_token of",
+                "    undefined -> undefined;",
+                "    _ -> Input",
+                "end");
     }
 }
