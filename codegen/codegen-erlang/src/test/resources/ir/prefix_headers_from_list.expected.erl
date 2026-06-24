1a165c0ddd6dd490d4b48fe10713154ed35f455f
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 16:42:47 +0200

    test: refresh prefix_headers_from_list golden for comprehension guards

--- codegen/codegen-erlang/src/test/resources/ir/prefix_headers_from_list.expected.erl
+++ codegen/codegen-erlang/src/test/resources/ir/prefix_headers_from_list.expected.erl
@@ -2,7 +2,7 @@ prefix_headers_from_list(Headers, Prefix) ->
     Map = maps:from_list([
 {binary:part(Name, byte_size(Prefix)), Val}
  || {Name, Val} <- Headers,
-    byte_size(Name) > byte_size(Prefix)
+    byte_size(Name) > byte_size(Prefix),
     binary:part(Name, 0, byte_size(Prefix)) =:= Prefix
 ]),
     case maps:size(Map) of
