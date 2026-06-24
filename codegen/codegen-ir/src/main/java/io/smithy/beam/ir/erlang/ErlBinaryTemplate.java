6566ab846597939dcad3386cdc7d4b8d4a7ca2b0
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 11:45:08 +0200

    feat: add ErlBinaryTemplate IR node with lines and asString tests
    Binary template segment rendering for Erlang expression trees.

---
+++ codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlBinaryTemplate.java
@@ -0,0 +1,39 @@
+package io.smithy.beam.ir.erlang;
+
+import java.util.List;
+
+public final class ErlBinaryTemplate implements ErlExpr {
+    private final List<ErlBinarySegment> segments;
+
+    public ErlBinaryTemplate(List<ErlBinarySegment> segments) {
+        this.segments = List.copyOf(segments);
+    }
+
+    public static ErlBinaryTemplate binaryTemplate(ErlBinarySegment... segments) {
+        return new ErlBinaryTemplate(List.of(segments));
+    }
+
+    public List<ErlBinarySegment> segments() {
+        return segments;
+    }
+
+    @Override
+    public List<String> lines() {
+        StringBuilder sb = new StringBuilder("<<");
+        for (int i = 0; i < segments.size(); i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(segmentString(segments.get(i)));
+        }
+        sb.append(">>");
+        return List.of(sb.toString());
+    }
+
+    private static String segmentString(ErlBinarySegment segment) {
+        if (segment instanceof ErlBinaryText text) {
+            return text.asString();
+        }
+        return ((ErlBinaryExpr) segment).asString();
+    }
+}
---
+++ codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlBinaryTemplateTest.java
@@ -0,0 +1,55 @@
+package io.smithy.beam.ir.erlang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ErlBinaryTemplateTest {
+    @Test
+    void binaryTemplateWithBinaryTypeSpecLines() {
+        ErlBinaryTemplate template = ErlBinaryTemplate.binaryTemplate(
+                ErlBinaryExpr.expr(ErlCallLocal.callLocal("uri_encode", new ErlVar("Name")), "binary"));
+        assertThat(template.lines()).containsExactly("<<(uri_encode(Name))/binary>>");
+    }
+
+    @Test
+    void binaryTemplateWithBinaryTypeSpecAsString() {
+        ErlBinaryTemplate template = ErlBinaryTemplate.binaryTemplate(
+                ErlBinaryExpr.expr(ErlCallLocal.callLocal("uri_encode", new ErlVar("Name")), "binary"));
+        assertThat(template.asString()).isEqualTo("<<(uri_encode(Name))/binary>>");
+    }
+
+    @Test
+    void binaryTemplateWithTypeSpecLines() {
+        ErlBinaryTemplate template = ErlBinaryTemplate.binaryTemplate(
+                ErlBinaryExpr.expr(
+                        ErlCall.call("erlang", "crc32", new ErlVar("Body")),
+                        "32/big-unsigned-integer"));
+        assertThat(template.lines()).containsExactly("<<(erlang:crc32(Body)):32/big-unsigned-integer>>");
+    }
+
+    @Test
+    void binaryTemplateWithTypeSpecAsString() {
+        ErlBinaryTemplate template = ErlBinaryTemplate.binaryTemplate(
+                ErlBinaryExpr.expr(
+                        ErlCall.call("erlang", "crc32", new ErlVar("Body")),
+                        "32/big-unsigned-integer"));
+        assertThat(template.asString()).isEqualTo("<<(erlang:crc32(Body)):32/big-unsigned-integer>>");
+    }
+
+    @Test
+    void binaryTemplateLines() {
+        ErlBinaryTemplate template = ErlBinaryTemplate.binaryTemplate(
+                ErlBinaryText.text("/types/"),
+                ErlBinaryExpr.expr(ErlCallLocal.callLocal("uri_encode", new ErlVar("Name")), true));
+        assertThat(template.lines()).containsExactly("<<\"/types/\", (uri_encode(Name))/binary>>");
+    }
+
+    @Test
+    void binaryTemplateAsString() {
+        ErlBinaryTemplate template = ErlBinaryTemplate.binaryTemplate(
+                ErlBinaryText.text("/types/"),
+                ErlBinaryExpr.expr(ErlCallLocal.callLocal("uri_encode", new ErlVar("Name")), true));
+        assertThat(template.asString()).isEqualTo("<<\"/types/\", (uri_encode(Name))/binary>>");
+    }
+}
