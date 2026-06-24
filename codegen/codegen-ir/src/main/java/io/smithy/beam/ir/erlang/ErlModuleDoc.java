ae0bffe8a0facf1bac73d19d852567c9242cbb3a
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 10:28:27 +0200

    refactor: move doc-attribute rendering onto doc IR classes
    Inline ErlLayout.renderDocAttribute on ErlModuleDoc and ErlFunctionDoc.

--- codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlModuleDoc.java
+++ codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlModuleDoc.java
@@ -1,5 +1,6 @@
 package io.smithy.beam.ir.erlang;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class ErlModuleDoc implements ErlPreambleEntry {
@@ -19,7 +20,21 @@ public final class ErlModuleDoc implements ErlPreambleEntry {
 
     @Override
     public List<String> lines(int indent) {
-        return ErlLayout.renderDocAttribute("moduledoc", text, indent);
+        return renderDocAttribute("moduledoc", text, indent);
+    }
+
+    private static List<String> renderDocAttribute(String attribute, String text, int indent) {
+        String head = IrObject.indent(indent) + "-" + attribute;
+        if (!text.contains("\n")) {
+            return List.of(head + " " + ErlString.string(text).asString() + ".");
+        }
+        List<String> out = new ArrayList<>();
+        out.add(head + " \"\"\"");
+        for (String line : text.split("\n", -1)) {
+            out.add(IrObject.indent(indent) + line);
+        }
+        out.add(IrObject.indent(indent) + "\"\"\".");
+        return out;
     }
 
     @Override
