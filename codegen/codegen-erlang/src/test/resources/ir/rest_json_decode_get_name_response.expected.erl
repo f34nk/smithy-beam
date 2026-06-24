8825be3e56d960cc7058afea691956f8e98846f0
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 16:42:48 +0200

    test: refresh REST JSON decode golden for compact match-case layout

--- codegen/codegen-erlang/src/test/resources/ir/rest_json_decode_get_name_response.expected.erl
+++ codegen/codegen-erlang/src/test/resources/ir/rest_json_decode_get_name_response.expected.erl
@@ -1,15 +1,14 @@
 -doc "Decode HTTP response for smithy.beam.demo.http#GetName.".
 -spec decode_get_name_response(#http_response{}) -> {'ok', get_name_output()} | {'error', term()}.
 decode_get_name_response(#http_response{status = 200, headers = Headers, body = Body}) ->
-    Decoded =
-        case Body of
-            <<>> -> #{};
-            _ ->
-                case jsone:try_decode(Body) of
-                    {ok, Val, _} -> Val;
-                    {error, _} -> #{}
-                end
-        end,
+    Decoded = case Body of
+        <<>> -> #{};
+        _ ->
+            case jsone:try_decode(Body) of
+                {ok, Val, _} -> Val;
+                {error, _} -> #{}
+            end
+    end,
     {ok, #get_name_output{
         name = maps:get(<<"name">>, Decoded, undefined)
     }};
