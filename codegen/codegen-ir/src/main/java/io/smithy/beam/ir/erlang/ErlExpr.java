82a69262bfc6f7c5d4ad211796537bef63b6efec
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 11:57:08 +0200

    feat: add ErlExprBlock for comma-separated body sequences with lines and asString tests

--- codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlExpr.java
+++ codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlExpr.java
@@ -3,4 +3,4 @@ package io.smithy.beam.ir.erlang;
 public sealed interface ErlExpr extends IrObject
         permits ErlAtom, ErlVar, ErlInteger, ErlString, ErlBinary, ErlCall, ErlCallLocal, ErlRecord,
                 ErlTuple, ErlList, ErlMap, ErlRecordAccess, ErlRecordUpdate, ErlCase, ErlMatch, ErlFun,
-                ErlOp, ErlApply, ErlTry, ErlListComprehension, ErlBinaryTemplate {}
+                ErlOp, ErlApply, ErlTry, ErlListComprehension, ErlBinaryTemplate, ErlExprBlock {}
---
+++ codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlExprBlockTest.java
@@ -0,0 +1,26 @@
+package io.smithy.beam.ir.erlang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ErlExprBlockTest {
+    private static ErlExprBlock matchInBlock() {
+        return ErlExprBlock.block(
+                ErlMatch.match(new ErlVarPattern("X"), ErlCallLocal.callLocal("fetch")),
+                new ErlVar("X"));
+    }
+
+    @Test
+    void matchInBlockLines() {
+        assertThat(matchInBlock().lines(1)).containsExactly(
+                "    X = fetch(),",
+                "    X");
+    }
+
+    @Test
+    void matchInBlockAsString() {
+        assertThat(matchInBlock().asString(1)).isEqualTo(
+                "    X = fetch(),\n    X");
+    }
+}
