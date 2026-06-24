196b1cb8dc6713f46a939ef16c71efa1d4eab2ed
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 11:41:09 +0200

    feat: add ErlRecordUpdate IR node with lines and asString tests
    Record update rendering delegated to ErlRecord with a base expression.

---
+++ codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlRecordUpdateTest.java
@@ -0,0 +1,30 @@
+package io.smithy.beam.ir.erlang;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ErlRecordUpdateTest {
+    @Test
+    void recordUpdateLines() {
+        ErlRecordUpdate update = ErlRecordUpdate.recordUpdate(
+                new ErlVar("Item"),
+                "basic_item",
+                ErlRecordField.field("name", new ErlBinary("x")));
+        assertThat(update.lines()).containsExactly(
+                "Item#basic_item{",
+                "    name = <<\"x\">>",
+                "}");
+    }
+
+    @Test
+    void recordUpdateAsString() {
+        ErlRecordUpdate update = ErlRecordUpdate.recordUpdate(
+                new ErlVar("Item"),
+                "basic_item",
+                ErlRecordField.field("name", new ErlBinary("x")));
+        assertThat(update.asString()).isEqualTo("Item#basic_item{\n    name = <<\"x\">>\n}");
+    }
+}
