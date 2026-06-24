22154d41aa84c2c44b10f07d8dc5e6da527cd964
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 11:39:48 +0200

    feat: add ErlRecordAccess IR node with lines and asString tests
    Record field access rendering for Erlang expression trees.

---
+++ codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlRecordAccessTest.java
@@ -0,0 +1,21 @@
+package io.smithy.beam.ir.erlang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ErlRecordAccessTest {
+    @Test
+    void recordAccessLines() {
+        ErlRecordAccess access = ErlRecordAccess.recordAccess(
+                new ErlVar("Item"), "basic_item", "name");
+        assertThat(access.lines()).containsExactly("Item#basic_item.name");
+    }
+
+    @Test
+    void recordAccessAsString() {
+        ErlRecordAccess access = ErlRecordAccess.recordAccess(
+                new ErlVar("Item"), "basic_item", "name");
+        assertThat(access.asString()).isEqualTo("Item#basic_item.name");
+    }
+}
