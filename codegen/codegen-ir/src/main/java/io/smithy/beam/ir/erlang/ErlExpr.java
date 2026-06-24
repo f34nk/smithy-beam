794018ad2543464fa38ba920e3f862fbefa68ffa
Frank Eickhoff <info@frankeickhoff.de>
2026-06-24 11:44:08 +0200

    feat: add ErlTry and ErlCatchClause IR nodes with lines and asString tests
    Multiline try/catch rendering for Erlang expression trees.

--- codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlExpr.java
+++ codegen/codegen-ir/src/main/java/io/smithy/beam/ir/erlang/ErlExpr.java
@@ -3,4 +3,4 @@ package io.smithy.beam.ir.erlang;
 public sealed interface ErlExpr extends IrObject
         permits ErlAtom, ErlVar, ErlInteger, ErlString, ErlBinary, ErlCall, ErlCallLocal, ErlRecord,
                 ErlTuple, ErlList, ErlMap, ErlRecordAccess, ErlRecordUpdate, ErlCase, ErlMatch, ErlFun,
-                ErlOp, ErlApply {}
+                ErlOp, ErlApply, ErlTry, ErlListComprehension {}
