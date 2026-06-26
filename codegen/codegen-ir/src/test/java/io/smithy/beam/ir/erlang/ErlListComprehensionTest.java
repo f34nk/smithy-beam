9d7348ff96a180ab29d9df923e1969f176f3a790
Frank Eickhoff <info@frankeickhoff.de>
2026-06-26 14:53:18 +0200

    chore: complete Erlang IR class migration test coverage
    Add paired lines and asString tests for ErlCapturedBlock and ErlMatchPattern,
    and extend list comprehension tests for filters and qualifier generators.

--- codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlListComprehensionTest.java
+++ codegen/codegen-ir/src/test/java/io/smithy/beam/ir/erlang/ErlListComprehensionTest.java
@@ -2,6 +2,8 @@ package io.smithy.beam.ir.erlang;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ErlListComprehensionTest {
@@ -21,5 +23,71 @@ class ErlListComprehensionTest {
                 new ErlVarPattern("X"),
                 new ErlVar("List"));
         assertThat(lc.asString()).isEqualTo("[decode(X) || X <- List]");
+    }
+
+    @Test
+    void comprehensionWithFiltersLines() {
+        ErlListComprehension lc = ErlListComprehension.comprehensionWithFilters(
+                ErlCallLocal.callLocal("decode", new ErlVar("X")),
+                new ErlVarPattern("X"),
+                new ErlVar("List"),
+                List.of(ErlOp.op("=/=", ErlVar.var("X"), ErlAtom.atom("null"))));
+        assertThat(lc.lines()).containsExactly("[decode(X) || X <- List, X =/= null]");
+    }
+
+    @Test
+    void comprehensionWithFiltersAsString() {
+        ErlListComprehension lc = ErlListComprehension.comprehensionWithFilters(
+                ErlCallLocal.callLocal("decode", new ErlVar("X")),
+                new ErlVarPattern("X"),
+                new ErlVar("List"),
+                List.of(ErlOp.op("=/=", ErlVar.var("X"), ErlAtom.atom("null"))));
+        assertThat(lc.asString()).isEqualTo("[decode(X) || X <- List, X =/= null]");
+    }
+
+    @Test
+    void comprehensionQualifiersLines() {
+        ErlListComprehension lc = ErlListComprehension.comprehensionQualifiers(
+                ErlTuple.tuple(
+                        ErlVar.var("Fun"),
+                        ErlCallLocal.callLocal("make_handler", ErlVar.var("Impl"), ErlVar.var("Fun"))),
+                List.of(
+                        new ErlComprehensionGenerator(
+                                ErlTuplePattern.tuplePattern(
+                                        ErlVarPattern.varPattern("Fun"),
+                                        ErlIntegerPattern.integerPattern(3)),
+                                ErlVar.var("Callbacks")),
+                        new ErlComprehensionFilter(ErlCall.call(
+                                "erlang",
+                                "function_exported",
+                                ErlVar.var("Impl"),
+                                ErlVar.var("Fun"),
+                                ErlInteger.integer(3)))));
+        assertThat(lc.lines()).containsExactly(
+                "[{Fun, make_handler(Impl, Fun)} || {Fun, 3} <- Callbacks,"
+                        + " erlang:function_exported(Impl, Fun, 3)]");
+    }
+
+    @Test
+    void comprehensionQualifiersAsString() {
+        ErlListComprehension lc = ErlListComprehension.comprehensionQualifiers(
+                ErlTuple.tuple(
+                        ErlVar.var("Fun"),
+                        ErlCallLocal.callLocal("make_handler", ErlVar.var("Impl"), ErlVar.var("Fun"))),
+                List.of(
+                        new ErlComprehensionGenerator(
+                                ErlTuplePattern.tuplePattern(
+                                        ErlVarPattern.varPattern("Fun"),
+                                        ErlIntegerPattern.integerPattern(3)),
+                                ErlVar.var("Callbacks")),
+                        new ErlComprehensionFilter(ErlCall.call(
+                                "erlang",
+                                "function_exported",
+                                ErlVar.var("Impl"),
+                                ErlVar.var("Fun"),
+                                ErlInteger.integer(3)))));
+        assertThat(lc.asString()).isEqualTo(
+                "[{Fun, make_handler(Impl, Fun)} || {Fun, 3} <- Callbacks,"
+                        + " erlang:function_exported(Impl, Fun, 3)]");
     }
 }
