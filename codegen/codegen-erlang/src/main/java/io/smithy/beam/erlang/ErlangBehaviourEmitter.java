d790925bdd17c19af182f3249d8838a3bf670ceb
Frank Eickhoff <info@frankeickhoff.de>
2026-07-05 09:36:49 +0200

    refactor: render behaviour modules through codec emission

--- codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/ErlangBehaviourEmitter.java
+++ codegen/codegen-erlang/src/main/java/io/smithy/beam/erlang/ErlangBehaviourEmitter.java
@@ -38,6 +38,6 @@ final class ErlangBehaviourEmitter {
     ErlangCodecEmission.writeModule(
         ctx,
         layout.behaviourModuleFile(),
-        ErlangBehaviourIr.behaviourModule(layout, service, builder.callbacks()));
+        ErlangBehaviourIr.behaviourModule(layout, service, builder.callbacks()).asString());
   }
 }
