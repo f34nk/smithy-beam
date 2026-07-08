37e094744f2d4802424ddfcbd2bd46c11c307bab
Frank Eickhoff <info@frankeickhoff.de>
2026-07-08 22:35:46 +0200

    feat: add endpoint host helper to utils
    Expose endpoint_host_from_config/1 for SigV4 signing and add specs with short docs for shared URL helpers.

--- runtime/erlang/src/utils.erl
+++ runtime/erlang/src/utils.erl
@@ -1,9 +1,12 @@
 %% Shared smithy-beam Erlang runtime helpers.
 -module(utils).
 -export([
-    split_base_url/1
+    split_base_url/1,
+    endpoint_host_from_config/1
 ]).
 
+%% @doc Split a base URL into scheme prefix and authority.
+-spec split_base_url(binary()) -> {binary(), binary()}.
 split_base_url(<<>>) ->
     {<<>>, <<>>};
 split_base_url(BaseUrl) ->
@@ -19,4 +22,23 @@ split_base_url(BaseUrl) ->
             >>};
         _ ->
             {<<>>, BaseUrl}
+    end.
+
+%% @doc Resolve the HTTP host from client config or AWS endpoint metadata.
+-spec endpoint_host_from_config(#{binary() => term()}) -> binary() | undefined.
+endpoint_host_from_config(Config) ->
+    case maps:get(base_url, Config, undefined) of
+        undefined ->
+            case
+                {
+                    maps:get(endpoint_prefix, Config, undefined),
+                    maps:get(region, Config, <<"us-east-1">>)
+                }
+            of
+                {undefined, _} -> undefined;
+                {Prefix, Region} -> <<Prefix/binary, ".", Region/binary, ".amazonaws.com">>
+            end;
+        BaseUrl ->
+            {_Scheme, Authority} = split_base_url(BaseUrl),
+            Authority
     end.
