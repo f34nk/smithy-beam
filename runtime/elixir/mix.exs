57ceb07d8022369acc3f7f922a1095998a6fb708
Frank Eickhoff <info@frankeickhoff.de>
2026-07-11 12:49:03 +0200

    feat: add static Elixir runtime modules
    Introduce shared RuntimeTypes, Utils, RuntimeHttp, AwsSigv4,
    HttpChecksum, and AwsEventStream under runtime/elixir.

---
+++ runtime/elixir/mix.exs
@@ -0,0 +1,21 @@
+defmodule SmithyBeamRuntime.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :smithy_beam_runtime,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    [
+      {:req, "~> 0.5", only: :test},
+      {:aws_signature, "~> 0.3.2", only: :test},
+      {:jason, "~> 1.4", only: :test}
+    ]
+  end
+end
