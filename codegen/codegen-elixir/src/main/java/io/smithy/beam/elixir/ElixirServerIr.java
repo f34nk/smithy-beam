81f97670212881aeee194fbc13fc4780eadaa4d6
Frank Eickhoff <info@frankeickhoff.de>
2026-07-14 14:23:57 +0200

    refactor: migrate Elixir server shell IR to beam-ir
    Server dispatcher modules now compose beam-ir Module records with
    behaviour, alias, and module attribute strings.

--- codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirServerIr.java
+++ codegen/codegen-elixir/src/main/java/io/smithy/beam/elixir/ElixirServerIr.java
@@ -1,19 +1,10 @@
 package io.smithy.beam.elixir;
 
+import io.beam.ir.elixir.Alias;
+import io.beam.ir.elixir.Function;
+import io.beam.ir.elixir.Moduledoc;
+import io.beam.ir.elixir.Module;
 import io.smithy.beam.core.BeamElixirLayout;
-import io.smithy.beam.ir.elixir.ExAliasAttr;
-import io.smithy.beam.ir.elixir.ExAtom;
-import io.smithy.beam.ir.elixir.ExBehaviourAttr;
-import io.smithy.beam.ir.elixir.ExComment;
-import io.smithy.beam.ir.elixir.ExFunction;
-import io.smithy.beam.ir.elixir.ExModule;
-import io.smithy.beam.ir.elixir.ExModuleAssignAttr;
-import io.smithy.beam.ir.elixir.ExModuleAttribute;
-import io.smithy.beam.ir.elixir.ExModuleEntry;
-import io.smithy.beam.ir.elixir.ExModuledoc;
-import io.smithy.beam.ir.elixir.ExPreambleEntry;
-import io.smithy.beam.ir.elixir.ExTuple;
-import io.smithy.beam.ir.elixir.ExVar;
 import java.util.ArrayList;
 import java.util.List;
 import software.amazon.smithy.model.shapes.ServiceShape;
@@ -21,36 +12,36 @@ import software.amazon.smithy.model.shapes.ServiceShape;
 final class ElixirServerIr {
   private ElixirServerIr() {}
 
-  static ExModule serverModule(
+  static Module serverModule(
       BeamElixirLayout layout,
       ServiceShape service,
       String behaviourMod,
       String typesMod,
-      List<ExFunction> operationFunctions,
-      List<ExFunction> discoveryFunctions) {
+      List<Function> operationFunctions,
+      List<Function> discoveryFunctions) {
     String serverMod = ElixirSymbolProvider.toModuleName(layout.serverModuleName());
     String implMod = ElixirSymbolProvider.toModuleName(layout.implModuleName());
-    List<ExPreambleEntry> preamble =
-        List.of(
-            ExModuledoc.moduledoc(
-                "Generated Elixir server dispatcher for "
-                    + service.getId()
-                    + ".\n\nDiscovers impl callbacks at startup via init_handlers/0."));
-    List<ExModuleAttribute> attributes =
-        List.of(
-            ExBehaviourAttr.behaviour(behaviourMod),
-            ExAliasAttr.alias(typesMod, "Types"),
-            ExAliasAttr.alias(behaviourMod, "Behaviour"),
-            ExModuleAssignAttr.assign("default_impl", ExVar.var(implMod)),
-            ExModuleAssignAttr.assign(
-                "handlers_key", ExTuple.tuple(ExVar.var(serverMod), ExAtom.atom("handlers"))));
-    List<ExFunction> functions = new ArrayList<>(operationFunctions);
+    List<Function> functions = new ArrayList<>(operationFunctions);
     functions.addAll(discoveryFunctions);
-    List<ExModuleEntry> epilogue =
+    return new Module(
+        serverMod,
+        Moduledoc.of(
+            "Generated Elixir server dispatcher for "
+                + service.getId()
+                + ".\n\nDiscovers impl callbacks at startup via init_handlers/0."),
+        List.of(),
         List.of(
-            ExComment.comment(
-                "Call " + serverMod + ".init_handlers/0 during application start before dispatch."),
-            ExComment.comment("Default impl module: " + implMod + "."));
-    return ExModule.module(serverMod, preamble, attributes, List.of(), functions, epilogue);
+            Alias.of(typesMod, "Types"),
+            Alias.of(behaviourMod, "Behaviour")),
+        List.of(
+            "@behaviour " + behaviourMod,
+            "@default_impl " + implMod,
+            "@handlers_key {" + serverMod + ", :handlers}",
+            "# Call " + serverMod + ".init_handlers/0 during application start before dispatch.",
+            "# Default impl module: " + implMod + "."),
+        List.of(),
+        List.of(),
+        List.of(),
+        functions);
   }
 }
