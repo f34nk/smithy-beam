44fcd5f60c0b6440a56bc200410d4fea86931b31
Frank Eickhoff <info@frankeickhoff.de>
2026-09-21 12:58:52 +0200

    test: migrate example HTTP mocks to client behaviour
    Replace Req-shaped request/1 test doubles with modules that implement
    RuntimeHttpClient across examples and AWS demos.

--- aws-examples/elixir/minimal-s3-demo/test/support/head_bucket_http_mock.ex
+++ aws-examples/elixir/minimal-s3-demo/test/support/head_bucket_http_mock.ex
@@ -1,5 +1,9 @@
 defmodule HeadBucketHttpMock do
   @moduledoc false
+  @behaviour RuntimeHttpClient
+
+  alias RuntimeHttpClient.Request
+  alias RuntimeTypes.HttpResponse
 
   @responses_key {__MODULE__, :responses}
   @count_key {__MODULE__, :count}
@@ -14,23 +18,25 @@ defmodule HeadBucketHttpMock do
     :persistent_term.get(@count_key, 0)
   end
 
-  def request(req_opts) do
-    method = Keyword.fetch!(req_opts, :method)
-    url = Keyword.fetch!(req_opts, :url)
+  @impl RuntimeHttpClient
+  def request(%Request{method: :head, url: url}) do
+    if String.ends_with?(url, "/my-bucket") do
+      count = :persistent_term.get(@count_key, 0)
+      :persistent_term.put(@count_key, count + 1)
 
-    unless method == :head and String.ends_with?(url, "/my-bucket") do
-      raise "unexpected request: #{inspect(req_opts)}"
+      case Enum.at(:persistent_term.get(@responses_key, []), count) do
+        {:status, status} ->
+          {:ok, %HttpResponse{status: status, headers: [], body: ""}}
+
+        nil ->
+          {:error, {:no_mock_response, count}}
+      end
+    else
+      {:error, {:unexpected_request, url}}
     end
+  end
 
-    count = :persistent_term.get(@count_key, 0)
-    :persistent_term.put(@count_key, count + 1)
-
-    case Enum.at(:persistent_term.get(@responses_key, []), count) do
-      {:status, status} ->
-        {:ok, %{status: status, headers: [], body: ""}}
-
-      nil ->
-        {:error, {:no_mock_response, count}}
-    end
+  def request(%Request{} = req) do
+    {:error, {:unexpected_request, req}}
   end
 end
